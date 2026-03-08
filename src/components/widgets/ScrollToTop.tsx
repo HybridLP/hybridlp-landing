654b48add30251a3031a190d5abebdc4f69f2dc9
Lanhubs <muhammedolanrewaju56@gmail.com>
2026-03-08 17:03:31 +0100

    fixed the arrow

--- src/components/widgets/ScrollToTop.tsx
+++ src/components/widgets/ScrollToTop.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { ChevronUp } from "lucide-react";
+import { ArrowUp } from "lucide-react";
 
 export default function ScrollToTop() {
   const [isVisible, setIsVisible] = useState(false);
@@ -29,10 +29,10 @@ export default function ScrollToTop() {
       {isVisible && (
         <button
           onClick={scrollToTop}
-          className="fixed bottom-8 right-8 z-[100] p-4 rounded-full bg-linear-to-br from-[#C8A702] via-[#A97D00] to-[#824E00] text-white shadow-[0_8px_30px_rgb(0,0,0,0.12)] hover:shadow-[0_20px_50px_rgba(0,0,0,0.2)] hover:scale-110 active:scale-95 transition-all duration-300 border-2 border-white/20 cursor-pointer group"
+          className="fixed bottom-8 right-8 z-[100] p-2 rounded-full bg-linear-to-br from-[#C8A702]/50 via-[#A97D00]/50 to-[#824E00]/50  text-white shadow-[0_8px_30px_rgb(0,0,0,0.12)] hover:shadow-[0_20px_50px_rgba(0,0,0,0.2)] hover:scale-110 active:scale-95 transition-all duration-300 border-2 border-white/20 cursor-pointer group"
           aria-label="Scroll to top"
         >
-          <ChevronUp className="w-6 h-6 group-hover:-translate-y-1 transition-transform duration-300" />
+          <ArrowUp className="w-4 h-4 group-hover:-translate-y-1 transition-transform duration-300" />
         </button>
       )}
     </>
