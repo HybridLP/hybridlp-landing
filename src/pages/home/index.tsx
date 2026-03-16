ee0add58de4afb80b663debd1d29510fee8412df
Lanhubs <muhammedolanrewaju56@gmail.com>
2026-03-16 14:24:01 +0100

    ui changes made

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -81,7 +81,7 @@ export default function Home() {
   }, [location]);
 
   return (
-    <div className="flex flex-col bg-[#000A1D] w-screen overflow-x-hidden">
+    <div className="flex flex-col bg-[#000A1D] w-full overflow-x-hidden relative">
       <SEO structuredData={[organizationSchema, sitelinksSchema]} />
       <Header />
       <Outlet />
