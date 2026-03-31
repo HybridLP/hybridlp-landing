bacc9c7e1c3423a615c3a5fb388459202ce6ebb0
Lanhubs <muhammedolanrewaju56@gmail.com>
2026-03-31 15:05:59 +0100

    fixes: font sizing on home pages

--- src/pages/home/components/about.tsx
+++ src/pages/home/components/about.tsx
@@ -27,12 +27,12 @@ export default () => {
 
   return (
     <>
-      <div className="flex py-8 md:py-12 lg:pt-16 flex-col gap-6 md:gap-8 lg:gap-10  text-white px-4 md:px-8 lg:px-32 xl:px-32 lg:min-h-screen">
-        <h1 className="inter-font text-white mb-4 mt-4 md:mt-12 text-[24px] font-semibold text-center md:text-[40px]">
+      <div className="flex  sm:py-8 pt-0 sm:pt-4 md:py-12 lg:pt-16 flex-col gap-6 md:gap-8 lg:gap-10  text-white px-4 md:px-8 lg:px-32 xl:px-32 lg:min-h-screen">
+        <h1 className="inter-font text-white mb-4 sm:mt-4 md:mt-12 text-[24px] font-semibold text-center md:text-[40px]">
           How It Works
         </h1>
         {/* Mobile Layout - Stacked */}
-        <div className="block lg:hidden mt-6">
+        <div className="block lg:hidden  sm:mt-6">
           {/* Image Section - Mobile */}
           <div className="relative mb-6">
             <SlideInSection
